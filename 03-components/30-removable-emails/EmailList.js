e89a8b6ff104235b8257d9d6be789d096257547a
winolog <IKlemin@beeline.ru>
2024-12-27 18:07:04 +0300

    30-removable-emails

--- 03-components/30-removable-emails/EmailList.js
+++ 03-components/30-removable-emails/EmailList.js
@@ -1,28 +1,33 @@
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue/dist/vue.esm-bundler.js'
 import EmailListItem from './EmailListItem.js'
 
 export default defineComponent({
-  name: 'EmailList',
+    name: 'EmailList',
 
-  components: {
-    EmailListItem,
-  },
-
-  props: {
-    emails: {
-      type: Array,
-      required: true,
+    components: {
+        EmailListItem,
     },
-  },
 
-  template: `
-    <ul class="emails-list unstyled-list" aria-label="Emails">
+    props: {
+        emails: {
+            type: Array,
+            required: true,
+        },
+
+    },
+
+    emits: ['deleteElement'],
+
+    template: `
+      <ul class="emails-list unstyled-list" aria-label="Emails">
       <EmailListItem
-        v-for="({ email, isMarked }, index) in emails"
-        :key="email"
-        :email="email"
-        :marked="isMarked"
+          v-for="({ email, isMarked }, index) in emails"
+          :key="email"
+          :email="email"
+          :index
+          :marked="isMarked"
+          @remove-element="$emit('delete-element', index)"
       />
-    </ul>
-  `,
-})
+      </ul>
+    `,
+});
